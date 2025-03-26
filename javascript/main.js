9899777e125c9c254b7ef5e4b2fcfcd2cee274ce
hirox1206 <ymgshrk.126@gmail.com>
2025-03-26 22:59:48 +0900

    feat: ToDoリストに項目を追加する処理を追加

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("Hello World!");
+const addTodoHandler = () => {
+  const todoValue = document.getElementById("inputTodo").value;
+  const listItem = document.createElement("li");
+  listItem.textContent = todoValue;
+
+  const todoList = document.getElementById("todoList");
+  todoList.appendChild(listItem);
+};
+
+const addTodoButton = document.getElementById("addTodoButton");
+addTodoButton.addEventListener("click", addTodoHandler, false);
