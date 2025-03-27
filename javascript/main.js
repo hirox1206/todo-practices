01d3594203c1710fe02ece20d958df8e8a0be576
hirox1206 <ymgshrk.126@gmail.com>
2025-03-27 14:08:26 +0900

    feat: 項目の削除機能を追加

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,14 @@
 "use strict";
 
+let i = 0;
+
 const addTodoHandler = () => {
   const todoValue = document.getElementById("inputTodo").value;
   const listItem = document.createElement("li");
   const listItemValue = document.createElement("p");
   const removeButton = document.createElement("button");
+  listItem.setAttribute("id", `listItem${i}`);
+  removeButton.setAttribute("id", `removeTodoButton${i}`);
 
   listItemValue.textContent = todoValue;
   removeButton.textContent = "削除";
@@ -14,6 +18,20 @@ const addTodoHandler = () => {
 
   const todoList = document.getElementById("todoList");
   todoList.appendChild(listItem);
+
+  setRemoveTodoHandler(i);
+  i++;
+};
+
+const removeTodoHandler = (i) => {
+  let listItem = document.getElementById(`listItem${i}`);
+  const todoList = document.getElementById("todoList");
+  todoList.removeChild(listItem);
+};
+
+const setRemoveTodoHandler = (i) => {
+  const removeButton = document.getElementById(`removeTodoButton${i}`);
+  removeButton.addEventListener("click", () => removeTodoHandler(i), false);
 };
 
 const addTodoButton = document.getElementById("addTodoButton");
