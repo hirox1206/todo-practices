c742f059fe09c9d0c9c8b5909ddfc4e19e1498fd
hirox1206 <ymgshrk.126@gmail.com>
2025-04-14 16:47:46 +0900

    feat: todoリストに項目を追加する処理を追加

--- typescript/dist/main.js
+++ typescript/dist/main.js
@@ -1,2 +1,45 @@
 "use strict";
-console.log("hello");
+class Todo {
+    constructor(content) {
+        this.content = content;
+        this.addTodo(content);
+    }
+    addTodo(content) {
+        const todoList = document.querySelector(".todo-app__list");
+        const todoItemDivElement = document.createElement("div");
+        todoItemDivElement.setAttribute("class", "todo-app__item");
+        const checkboxInputElement = document.createElement("input");
+        checkboxInputElement.setAttribute("type", "checkbox");
+        checkboxInputElement.setAttribute("class", "todo-app__checkbox");
+        const todoContentLabelElement = document.createElement("label");
+        todoContentLabelElement.setAttribute("class", "todo-app__label");
+        todoContentLabelElement.textContent = content;
+        todoItemDivElement === null || todoItemDivElement === void 0 ? void 0 : todoItemDivElement.appendChild(checkboxInputElement);
+        todoItemDivElement === null || todoItemDivElement === void 0 ? void 0 : todoItemDivElement.appendChild(todoContentLabelElement);
+        todoList === null || todoList === void 0 ? void 0 : todoList.appendChild(todoItemDivElement);
+    }
+}
+class TodoList {
+    static getInstance() {
+        if (!TodoList.instance) {
+            new TodoList();
+        }
+        return TodoList.instance;
+    }
+    constructor() {
+        const addTodoButoon = document.querySelector(".todo-app__add-button");
+        if (addTodoButoon) {
+            addTodoButoon.addEventListener("click", this.addTodoEventHandler, false);
+        }
+    }
+    addTodoEventHandler() {
+        const todoInput = document.querySelector(".todo-app__input");
+        if (!todoInput)
+            return;
+        const todoContent = todoInput.value;
+        if (!todoContent.trim())
+            return;
+        new Todo(todoContent);
+    }
+}
+TodoList.getInstance();
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,1 +1,57 @@
-console.log("hello");
+class Todo {
+  constructor(private content: string) {
+    this.addTodo(content);
+  }
+
+  private addTodo(content: string) {
+    const todoList = document.querySelector<HTMLDivElement>(".todo-app__list");
+
+    const todoItemDivElement = document.createElement("div");
+    todoItemDivElement.setAttribute("class", "todo-app__item");
+
+    const checkboxInputElement = document.createElement("input");
+    checkboxInputElement.setAttribute("type", "checkbox");
+    checkboxInputElement.setAttribute("class", "todo-app__checkbox");
+
+    const todoContentLabelElement = document.createElement("label");
+    todoContentLabelElement.setAttribute("class", "todo-app__label");
+    todoContentLabelElement.textContent = content;
+
+    todoItemDivElement?.appendChild(checkboxInputElement);
+    todoItemDivElement?.appendChild(todoContentLabelElement);
+    todoList?.appendChild(todoItemDivElement);
+  }
+}
+
+class TodoList {
+  private static instance: TodoList;
+
+  static getInstance() {
+    if (!TodoList.instance) {
+      new TodoList();
+    }
+    return TodoList.instance;
+  }
+
+  private constructor() {
+    const addTodoButoon = document.querySelector<HTMLButtonElement>(
+      ".todo-app__add-button"
+    );
+    if (addTodoButoon) {
+      addTodoButoon.addEventListener("click", this.addTodoEventHandler, false);
+    }
+  }
+
+  private addTodoEventHandler() {
+    const todoInput =
+      document.querySelector<HTMLInputElement>(".todo-app__input");
+    if (!todoInput) return;
+
+    const todoContent = todoInput.value!;
+    if (!todoContent.trim()) return;
+
+    new Todo(todoContent);
+  }
+}
+
+TodoList.getInstance();
